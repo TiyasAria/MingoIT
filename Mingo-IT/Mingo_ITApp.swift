3aaafbf80de216e5585105a26eec53f171bf0d7d
Zai <zaidan20ti@mahasiswa.pcr.ac.id>
2023-05-31 11:01:53 +0700

    Initial Commit

---
+++ Mingo-IT/Mingo_ITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mingo_ITApp.swift
+//  Mingo-IT
+//
+//  Created by Zai on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Mingo_ITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
