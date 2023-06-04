201be7fd5de8039a5e778f3b0c1685db6bc224f7
Zai <zaidan20ti@mahasiswa.pcr.ac.id>
2023-06-04 10:11:05 +0700

    Complete & Incomplete Lesson View

---
+++ Mingo-IT/CompleteLessonView.swift
@@ -0,0 +1,51 @@
+//
+//  CompleteLessonView.swift
+//  Mingo-IT
+//
+//  Created by Zai on 04/06/23.
+//
+
+import SwiftUI
+
+struct CompleteLessonView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Image("CompleteOwl")
+            
+            VStack(spacing: 10) {
+                Text("Great Job !")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text("Congratulations, you conquer this \nmaterial well.")
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            
+            HStack {
+                Image(systemName: "desktopcomputer")
+                Text("Programming + 3")
+            }
+            .frame(width: 310)
+            .padding(15)
+            .font(.title3)
+            .fontWeight(.bold)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.init(hex: "54AAB4"), lineWidth: 2)
+            )
+            
+            Spacer()
+            
+            MButton(text: "Continue", isFullWidth: true, background: Color.init(hex: "FF8C41"))
+        }
+    }
+}
+
+struct CompleteLessonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteLessonView()
+    }
+}
