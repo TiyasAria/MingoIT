0975ec379367fa68e647fdbe13f2614e0660279d
Zai <zaidan20ti@mahasiswa.pcr.ac.id>
2023-06-04 09:48:27 +0700

    Disclaimer View

--- Mingo-IT/DisclaimerView.swift
+++ Mingo-IT/DisclaimerView.swift
@@ -11,7 +11,26 @@ struct DisclaimerView: View {
     var body: some View {
         VStack {
             Spacer()
-            MButton(text: "I Understand", isFullWidth: true)
+            Group {
+                ZStack(alignment: .topLeading) {
+                    Image("BubbleChat")
+                        .scaleEffect(1.175)
+                        .background(
+                            Image("BubbleChat")
+                                .scaleEffect(1.175)
+                                .shadow(color: .black,
+                                        radius: 0, x: 0, y: 4)
+                                .opacity(0.25)
+                        )
+                    Text("Hello , Welcome to Mingo . This application is designed with the aim of providing a learning simulation of the world of lectures.\n\nHowever, please note that the app does not replace actual university education or guarantee real-life outcomes.")
+                        .offset(x: 10)
+                        .foregroundColor(.white)
+                        .frame(width: 275.0)
+                }
+                Image("DisclaimerOwl")
+            }
+            Spacer()
+            MButton(text: "I Understand", isFullWidth: true, background: Color.init(hex: "FF8C41"))
         }
     }
 }
